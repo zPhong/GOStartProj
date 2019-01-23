6a880347181c0bbc1540198dfccd35065c3a796c
phong <luctruongphong13@gmail.com>
2019-01-23 15:37:40 +0700

    Fix flow babel error

--- app/App.js
+++ app/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,16 +7,17 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload, Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,Shake or press menu button for dev menu'
+  android:
+    'Double tap R on your keyboard to reload,Shake or press menu button for dev menu'
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component<Props> {
   render() {
     return (
       <View style={styles.container}>
